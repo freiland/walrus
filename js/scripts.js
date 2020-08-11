83acebf4dd03f31d423bfcbe8f249143d29cb0b0
Forrest Reiland <you@example.com>
2020-08-11 11:29:09 -0700

    add interactive button to change page from light to dark or dark to light

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-  $("p".click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $("clickable".click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+    $("#sample-hide").toggle();
+    $("body").addClass("light-background")
   });
 });
